74a1b54eae97db3595435beef019a91b1589e674
theyjxie@gmail.com <yijun.xie@uwaterloo.ca>
2022-02-15 14:46:06 -0500

    try to add parallel

--- src/ProjectionPursuit.jl
+++ src/ProjectionPursuit.jl
@@ -1,19 +1,26 @@
 module ProjectionPursuit
 
+using Distributed
 include("sphere_optimize.jl")
 
 # Write your package code here.
 
-function Projection_Pursuit(data::Matrix{Float64}, object_fun::Function; n_of_candidate::Int64=5 ,unit_sphere=nothing, fnscale::Int64=-1)
+function Projection_Pursuit(data::Matrix{Float64}, object_fun::Function; n_of_candidate::Int64=5 ,unit_sphere=nothing, fnscale::Int64=-1, par::Bool=true)
     n, p = size(data)
     if isnothing(unit_sphere)
         unit_sphere= GenSphere(n^2, p)
     end
     r = map(x->object_fun(data, Vector(x)), eachrow(unit_sphere))
     proj_dirs = hcat(unit_sphere, r)
-    proj_dirs[sortperm(proj_dirs[:, p+1], rev=true), :]
+    proj_dirs = proj_dirs[sortperm(proj_dirs[:, p+1], rev=true), :]
     candidate_dirs = proj_dirs[1:n_of_candidate, 1:p]
-    opt_objs = map(x->Sphere_Optimize(data, Vector(x), object_fun;fnscale), eachrow(candidate_dirs))
+    if par
+        np = Distributed.nprocs()
+        Distributed.addprocs(np)
+        opt_objs = pmap(x->Sphere_Optimize(data, Vector(x), object_fun;fnscale), eachrow(candidate_dirs))
+    else
+        opt_objs = map(x->Sphere_Optimize(data, Vector(x), object_fun;fnscale), eachrow(candidate_dirs))
+    end
     max_var = maximum([o.var for o in opt_objs])
     s = randn(p)
     for o in collect(opt_objs)
