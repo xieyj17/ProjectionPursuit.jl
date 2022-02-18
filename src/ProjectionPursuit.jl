ece2c47c5a79553135a182bf028eda761de766f8
theyjxie@gmail.com <yijun.xie@uwaterloo.ca>
2022-02-18 14:47:26 -0500

    dimension reduction for multiple directions

--- src/ProjectionPursuit.jl
+++ src/ProjectionPursuit.jl
@@ -4,14 +4,19 @@ include("sphere_optimize.jl")
 
 # Write your package code here.
 
-function Projection_Pursuit(data::Matrix{Float64}, object_fun::Function; 
-    n_of_candidate::Int64=5 ,unit_sphere=nothing, fnscale::Int64=-1, par::Bool=true, fast::Bool=true)
+struct ProjectionPursuitRes
+    object_vals::Vector{Float64}
+    proj::Matrix{Float64}
+end
+
+function sphere_optimize(data::Matrix{Float64}, object_fun::Function; 
+    n_of_candidate::Int64=5 ,unit_sphere=nothing, proj=nothing, fnscale::Int64=-1, par::Bool=true, fast::Bool=true)
     n, p = size(data)
     if isnothing(unit_sphere)
         if fast
-            unit_sphere= FastGenSphere(n^2, p;par)
+            unit_sphere= fastgensphere(n^2, p;par)
         else
-            unit_sphere= GenSphere(n^2, p)
+            unit_sphere= gensphere(n^2, p)
         end
     end
 
@@ -28,16 +33,30 @@ function Projection_Pursuit(data::Matrix{Float64}, object_fun::Function;
     end
     proj_dirs = hcat(unit_sphere, r)
     proj_dirs = proj_dirs[sortperm(proj_dirs[:, p+1], rev=true), :]
-    candidate_dirs = proj_dirs[1:n_of_candidate, 1:p]    
-    if par 
-        opt_objs = Vector{Any}(undef, n_of_candidate)
-        @sync for i in 1:n_of_candidate
-            Threads.@spawn opt_objs[i] = Sphere_Optimize(data, Vector(candidate_dirs[i,:]), object_fun;fnscale)
+    candidate_dirs = proj_dirs[1:n_of_candidate, 1:p]
+    
+    if isnothing(proj)
+        if par 
+            opt_objs = Vector{Any}(undef, n_of_candidate)
+            @sync for i in 1:n_of_candidate
+                Threads.@spawn opt_objs[i] = sphere_optimize_emb(data, Vector(candidate_dirs[i,:]), object_fun;fnscale)
+            end
+            #opt_objs = pmap(x->Sphere_Optimize(pdata, Vector(x), pobject_fun;fnscale), eachrow(candidate_dirs))        
+        else
+            opt_objs = map(x->sphere_optimize_emb(data, Vector(x), object_fun;fnscale), eachrow(candidate_dirs))
         end
-        #opt_objs = pmap(x->Sphere_Optimize(pdata, Vector(x), pobject_fun;fnscale), eachrow(candidate_dirs))        
     else
-        opt_objs = map(x->Sphere_Optimize(data, Vector(x), object_fun;fnscale), eachrow(candidate_dirs))
-    end
+        if par 
+            opt_objs = Vector{Any}(undef, n_of_candidate)
+            @sync for i in 1:n_of_candidate
+                Threads.@spawn opt_objs[i] = sphere_optimize_emb(data, Vector(candidate_dirs[i,:]), object_fun, proj;fnscale)
+            end
+            #opt_objs = pmap(x->Sphere_Optimize(pdata, Vector(x), pobject_fun;fnscale), eachrow(candidate_dirs))        
+        else
+            opt_objs = map(x->sphere_optimize_emb(data, Vector(x), object_fun, proj;fnscale), eachrow(candidate_dirs))
+        end
+    end    
+    
     max_var = maximum([o.var for o in opt_objs])
     s = zeros(p)
     for o in collect(opt_objs)
@@ -45,11 +64,49 @@ function Projection_Pursuit(data::Matrix{Float64}, object_fun::Function;
             s = o.s
         end
     end
-    return Sphere_Optimize_Res(max_var, s)
+    return SphereOptimizeRes(max_var, s)
 end
 
 
-export Projection_Pursuit, Sphere_Optimize, ToSphere, FromSphere, GenSphere, FastGenSphere,
-        Sphere_Optimize_Res
+function projection_pursuit(data::Matrix{Float64}, object_fun::Function, outdim::Int64=1; 
+    n_of_candidate::Int64=5 ,unit_sphere=nothing, fnscale::Int64=-1, par::Bool=true, fast::Bool=true)
+
+    n, p = size(data)
+    if p < outdim
+        throw(ArgumentError("Output dimension is greater than data dimension."))
+    end
+
+    if isnothing(unit_sphere)
+        if fast
+            unit_sphere= fastgensphere(n^2, p;par)
+        else
+            unit_sphere= gensphere(n^2, p)
+        end
+    end
+
+    object_vals = Vector{Float64}(undef, outdim)
+    proj = Matrix{Float64}(undef, (p, outdim))
+
+    t_opt_res = sphere_optimize(data, object_fun;n_of_candidate, unit_sphere, fnscale, par, fast)
+    object_vals[1] = t_opt_res.var
+    ts = t_opt_res.s
+    proj[:,1] = ts
+    
+    if outdim > 1
+        for i in 2:outdim
+            unit_sphere = _residualsphere(unit_sphere, ts; par)
+            t_opt_res = sphere_optimize(data, object_fun;
+                n_of_candidate=n_of_candidate, unit_sphere=unit_sphere, proj=proj[:,1:(i-1)],fnscale=fnscale,
+                par=par, fast=fast)
+            object_vals[i] = t_opt_res.var
+            ts = t_opt_res.s
+            proj[:,i] = ts
+        end
+    end
+    return ProjectionPursuitRes(object_vals, proj)
+end
+
+export projection_pursuit, sphere_optimize, tosphere, fromsphere, gensphere, fastgensphere,
+       SphereOptimizeRes, ProjectionPursuitRes
 
 end
--- src/sphere_optimize.jl
+++ src/sphere_optimize.jl
@@ -3,13 +3,13 @@ using QuadGK
 using Roots
 using Optim
 
-function y1(x::Vector{Float64})::Matrix{Float64}
+function _y1(x::Vector{Float64})::Matrix{Float64}
     r1 = [cos(2*pi*s) for s in x]
     r2 = [sin(2*pi*s) for s in x]
     return hcat(r1, r2)
 end
 
-function y2(x::Vector{Float64}, y::Matrix{Float64})::Matrix{Float64}
+function _y2(x::Vector{Float64}, y::Matrix{Float64})::Matrix{Float64}
     ya = y[:,1]
     yb = y[:,2]
     p1a = [sqrt(1 - (1 - 2*r)^2) * s for (r,s) in zip(x, ya)]
@@ -18,31 +18,31 @@ function y2(x::Vector{Float64}, y::Matrix{Float64})::Matrix{Float64}
     return hcat(p1a, p1b, p2)
 end
 
-function b_inside(a::Float64, b::Float64)
+function _b_inside(a::Float64, b::Float64)
     function tf(u::Float64)
         return u^(a-1) * (1-u)^(b-1)
     end
     return tf
 end
 
-function bz(a::Float64, b::Float64, z::Float64)
-    tf = b_inside(a,b)
+function _bz(a::Float64, b::Float64, z::Float64)
+    tf = _b_inside(a,b)
     inte, err = quadgk(tf, 0, z, rtol=1e-3)
     return inte
 end
 
-function bd(a::Float64, b::Float64)
-    return bz(a,b,float(1))
+function _bd(a::Float64, b::Float64)
+    return _bz(a,b,float(1))
 end
 
-function hd(x::Float64, d::Float64)
-    ix = bz(d/2, d/2, x) / bd(d/2, d/2)
+function _hd(x::Float64, d::Float64)
+    ix = _bz(d/2, d/2, x) / _bd(d/2, d/2)
     return ix
 end
 
-function hd_inv(y::Float64, d::Int64)
+function _hd_inv(y::Float64, d::Int64)
     function tf(x::Float64)
-        return hd(x, Float64(d)) - y
+        return _hd(x, Float64(d)) - y
     end
     res = find_zero(tf, (0, 1))
     return res
@@ -50,7 +50,7 @@ end
 
 function yd(x::Vector{Float64}, y::Matrix{Float64}, d::Int64)
     nr, nc = size(y)
-    pr = [sqrt(1 - (1 - 2*hd_inv(r, d))^2) for r in x]
+    pr = [sqrt(1 - (1 - 2*_hd_inv(r, d))^2) for r in x]
     res = zeros(nr, nc)
     for i in 1:nc
         ty = y[:,i]
@@ -59,17 +59,17 @@ function yd(x::Vector{Float64}, y::Matrix{Float64}, d::Int64)
         #append!(res, tp)
     end
     #tres = reshape(res, (nr, nc))
-    p2 = [1 - 2*hd_inv(r, d) for r in x]
+    p2 = [1 - 2*_hd_inv(r, d) for r in x]
     return hcat(res, p2)
 end
 
 
 
-function GenSphere(N::Int64, d::Int64)::Matrix{Float64}
+function gensphere(N::Int64, d::Int64)::Matrix{Float64}
     s = SobolSeq(d-1)
     rds = hcat([next!(s) for i = 1:N] ...)'
-    prj1 = y1(rds[:,1])
-    prj2 = y2(rds[:,2], prj1)
+    prj1 = _y1(rds[:,1])
+    prj2 = _y2(rds[:,2], prj1)
     tp = prj2
     for j in 3:(d-1)
         np = yd(rds[:,j], tp, j)
@@ -78,25 +78,25 @@ function GenSphere(N::Int64, d::Int64)::Matrix{Float64}
     return tp
 end
 
-function FastGenSphere(N::Int64, d::Int64; par::Bool=true)::Matrix{Float64}
+function fastgensphere(N::Int64, d::Int64; par::Bool=true)::Matrix{Float64}
     eps=1e-10
     s = SobolSeq(d)
-    rds = hcat([next!(s) for i = 1:N] ...)'
+    rds = hcat([next!(s) for i = 1:(N+1)] ...)'
     rds = (rds .*2).-1 
     tp = Matrix{Float64}(undef, (N,d))
     if par
-        @sync for j in 1:N
-            Threads.@spawn tp[j,:] = (rds[j,:].+eps) ./ sqrt(sum(rds[j,:].^2).+eps)
+        @sync for j in 2:(N+1)
+            Threads.@spawn tp[(j-1),:] = (rds[j,:].+eps) ./ sqrt(sum(rds[j,:].^2).+eps)
         end
     else
-        for j in 1:N
-            tp[j,:] = (rds[j,:].+eps) ./ sqrt(sum(rds[j,:].^2).+eps)
+        for j in 2:(N+1)
+            tp[(j-1),:] = (rds[j,:].+eps) ./ sqrt(sum(rds[j,:].^2).+eps)
         end
     end
     return tp
 end
 
-function RegularizedTheta!(theta::Vector{Float64})
+function _regularizedtheta!(theta::Vector{Float64})
     n = length(theta)
     for i in 1:(n-1)
         while theta[i] < 0
@@ -110,7 +110,7 @@ function RegularizedTheta!(theta::Vector{Float64})
     theta[n] = theta[n] % π
 end
 
-function MedFromSphere(theta::Vector{Float64}, i::Int64)
+function _medfromsphere(theta::Vector{Float64}, i::Int64)
     res = 1
     if i <= length(theta)
         temp_theta = theta[1:i]
@@ -131,17 +131,17 @@ function MedFromSphere(theta::Vector{Float64}, i::Int64)
     return res
 end
 
-function FromSphere(theta::Vector{Float64})
+function fromsphere(theta::Vector{Float64})
     s = zeros(length(theta)+1)
-    RegularizedTheta!(theta)
+    _regularizedtheta!(theta)
     for i in 1:length(s)
-        s[i] = MedFromSphere(theta, i)
+        s[i] = _medfromsphere(theta, i)
     end
     return s
 end
 
 
-function ToSphere(s::Vector{Float64})
+function tosphere(s::Vector{Float64})
     n = length(s)
     theta = zeros(n-1)
     
@@ -157,19 +157,57 @@ function ToSphere(s::Vector{Float64})
     return theta
 end
 
+function _residualsphere(sphere::Matrix{Float64}, dir::Vector{Float64};par=true)
+    eps = 1e-10
+    proj_val = sphere * dir
+    residuals = Matrix{Float64}(undef, size(sphere))
+    for i in 1:size(sphere)[1]
+        residuals[i,:] = sphere[i,:] - proj_val[i] * dir
+    end
+    if par
+        @sync for j in 1:size(sphere)[1]
+            Threads.@spawn residuals[j,:] = (residuals[j,:].+eps) ./ sqrt(sum(residuals[j,:].^2).+eps)
+        end
+    else
+        for j in 1:size(sphere)[1]
+            residuals[j,:] = (residuals[j,:].+eps) ./ sqrt(sum(residuals[j,:].^2).+eps)
+        end
+    end
+    return residuals
+end
 
-struct Sphere_Optimize_Res
+function _orthogonal_dir(dir::Vector{Float64}, proj_dirs::Matrix{Float64})
+    proj_val = dir' * proj_dirs
+    for j in 1:length(proj_val)
+        dir = dir - proj_dirs[:,j]*proj_val[j]
+    end
+    return dir
+end
+
+
+struct SphereOptimizeRes
     var::Float64
     s::Vector{Float64}
 end
 
-function Sphere_Optimize(data::Matrix{Float64}, s::Vector{Float64}, object_fun::Function;fnscale::Int64=-1)
-    theta = ToSphere(s)
-    RegularizedTheta!(theta)
-    temp_fn(theta) = object_fun(data, FromSphere(theta))*fnscale
+function sphere_optimize_emb(data::Matrix{Float64}, s::Vector{Float64}, object_fun::Function;fnscale::Int64=-1)
+    theta = tosphere(s)
+    _regularizedtheta!(theta)
+    temp_fn(theta) = object_fun(data, fromsphere(theta))*fnscale
     k = optimize(temp_fn, theta, LBFGS())
     ntheta = Optim.minimizer(k)
-    RegularizedTheta!(ntheta) 
-    return Sphere_Optimize_Res(object_fun(data, FromSphere(ntheta)), FromSphere(ntheta))
+    _regularizedtheta!(ntheta) 
+    return SphereOptimizeRes(object_fun(data, fromsphere(ntheta)), fromsphere(ntheta))
+end
+
+function sphere_optimize_emb(data::Matrix{Float64}, s::Vector{Float64}, object_fun::Function, proj_dirs::Matrix{Float64};
+    fnscale::Int64=-1)
+    theta = tosphere(s)
+    _regularizedtheta!(theta)
+    temp_fn(theta) = object_fun(data, _orthogonal_dir(fromsphere(theta), proj_dirs))*fnscale
+    k = optimize(temp_fn, theta, LBFGS())
+    ntheta = Optim.minimizer(k)
+    _regularizedtheta!(ntheta) 
+    return SphereOptimizeRes(object_fun(data, fromsphere(ntheta)), fromsphere(ntheta))
 end
 
