1d0b5ffc32a5fc9cd91a313df1c26e05b7a8c32f
Documenter.jl <documenter@juliadocs.github.io>
2022-02-18 21:51:51 +0000

    build based on 5e57755

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProjectionPursuit","category":"page"},{"location":"#ProjectionPursuit","page":"Home","title":"ProjectionPursuit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProjectionPursuit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProjectionPursuit]","category":"page"}]
+}
