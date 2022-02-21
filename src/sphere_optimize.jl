6f64434f823369f90120c116223c5df7c8ed56c8
xieyj17 <yijun.xie@uwaterloo.ca>
2022-02-21 11:14:27 -0500

    add logo, test docstrings

--- src/sphere_optimize.jl
+++ src/sphere_optimize.jl
@@ -64,7 +64,22 @@ function yd(x::Vector{Float64}, y::Matrix{Float64}, d::Int64)
 end
 
 
+"""
+    gensphere(N::Int64, d::Int64)
 
+Generate low discrepancy sequences of length N on a d dimension unit sphere.
+
+Roughly speaking, a low discrepancy sequence is a sequence of samples of a random variable
+    has the property that the number of points within a region is proportional to the size
+    of the region.
+This function returns a N-by-d matrix, where each row represents a sample of a random 
+    vector that is uniformly distributed on a d-dimensional unit sphere.
+This function is based on Brauchart, Johann S., Josef Dick, and Lou Fang. 
+    "Spatial low-discrepancy sequences, spherical cone discrepancy,
+     and applications in financial modeling." 
+     Journal of Computational and Applied Mathematics 286 (2015): 28-53.
+
+"""
 function gensphere(N::Int64, d::Int64)::Matrix{Float64}
     s = SobolSeq(d-1)
     rds = hcat([next!(s) for i = 1:N] ...)'
